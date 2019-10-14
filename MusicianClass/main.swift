c567454774ac6915e3417e3e2d950b281236538a
Mehmet Turan <mmtrn@outlook.com>
2019-10-14 14:43:51 +0300

    Initial Commit

---
+++ MusicianClass/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MusicianClass
+//
+//  Created by Mehmet Turan on 10/14/19.
+//  Copyright © 2019 Mehmet Turan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
